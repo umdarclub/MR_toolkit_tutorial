7b1502274182514c61850872705bddb62057ee4d
Axel <haxelegide@yahoo.fr>
2018-04-03 17:16:06 -0400

    Finished Project

---
+++ UI-HoloLens/Assets/ColorChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+    public GameObject mySphere; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void colorBlue()
+    {
+        Renderer rend = mySphere.GetComponent<Renderer>();
+        rend.material.color = Color.blue;
+    }
+    public void colorGreen()
+    {
+        Renderer rend = mySphere.GetComponent<Renderer>();
+        rend.material.color = Color.green;
+    }
+
+    public void colorRed()
+    {
+        Renderer rend = mySphere.GetComponent<Renderer>();
+        rend.material.color = Color.red;
+    }
+
+}
---
+++ UI-HoloLens/Assets/Script/DisplayCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCube : MonoBehaviour {
+
+    public GameObject plane ;
+    private GameObject obj2; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void create_cube () {
+        obj2 = GameObject.Find("MyCube");
+        Transform obj_transfrom = plane.GetComponent<Transform>();
+        Vector3 pos = new Vector3(obj_transfrom.position.x, obj_transfrom.position.y / 2.0f, obj_transfrom.position.z);
+        
+        Instantiate(obj2, pos, Quaternion.identity);
+
+    }
+}
